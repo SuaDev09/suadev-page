c58b5011c9e51d822cdf012c14db4cf6d4e752ed
SuaDev09 <91593664+SuaDev09@users.noreply.github.com>
2023-08-21 22:37:55 -0700

    styles and css variables defined

--- src/app/utils/home.constants.ts
+++ src/app/utils/home.constants.ts
@@ -1,2 +1,14 @@
-export const H1_TITLE: string = `Hi, I'm Francisco Suarez`;
+export const H1_TITLE: string = `Hi, I'm`;
 export const P_CONTENT: string = `I'm a self-taught product developer, solopreneur, and educator. I love to write and create content, and I build websites that are fast, responsive, intuitive, and dynamic. You've found my secret portfolio — everything you want to know and more is here.`;
+export const SERVICES_ITEMS: string[] = [
+  'Web Development',
+  'Web Design',
+  'Content Creation',
+  'SEO',
+  'Digital Marketing',
+  'Social Media Marketing',
+  'Graphic Design',
+  'Video Editing',
+  'Photography',
+  'Copywriting',
+];
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
 // Constants
-import { H1_TITLE, P_CONTENT } from '../../utils/home.constants';
+import {
+  H1_TITLE,
+  P_CONTENT,
+  SERVICES_ITEMS,
+} from '../../utils/home.constants';
 
 @Component({
   selector: 'app-home',
@@ -11,4 +15,6 @@ import { H1_TITLE, P_CONTENT } from '../../utils/home.constants';
 export class HomeComponent {
   H1_TITLE = H1_TITLE;
   P_CONTENT = P_CONTENT;
+  IMG_PROFILE = '../../.././assets/icons/2.svg';
+  SERVICES_ITEMS = SERVICES_ITEMS;
 }
